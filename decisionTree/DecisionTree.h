5f7795805ec1410c438f15119000d43750a7d0d1
reece <gemini_c6@163.com>
2022-10-10 17:30:40 +0800

    trainData模块修改 & data.txt数据添加

--- decisionTree/DecisionTree.h
+++ decisionTree/DecisionTree.h
@@ -1,1 +1,24 @@
+#ifndef DECISIONTREE
+#define DECISIONTRE
 #include<vector>
+#include<math.h>
+using namespace std;
+
+
+
+//training data 训练所用数据
+class TrainData{
+public:
+    vector<vector<int>> Input;    //一行一条数据
+    vector<int> Output;    //允许离散的输出值
+    void InsertData(vector<int> data, int out){
+        //输入一行数据和目标输出
+        Input.push_back(data);    //特征
+        Output.push_back(out);    //标签label
+    }
+};
+
+
+
+
+#endif
