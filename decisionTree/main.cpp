aeb4221e896f22b20e5e23e598e42e934019e93b
reece <gemini_c6@163.com>
2022-10-12 20:21:43 +0800

    添加main.cpp

---
+++ decisionTree/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include"DecisionTree.h"
+using namespace std;
+
+int main(){
+    Tree* tree = new Tree;
+    cout<< "Trained........."<<endl;
+    while(True){
+        tree->GetOutput();
+    }
+    return 0;
+}
+
+
