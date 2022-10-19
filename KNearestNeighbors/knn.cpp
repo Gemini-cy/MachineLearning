dc35518d2fe66f72b69fed47b2483f3cab0c39c8
reece <gemini_c6@163.com>
2022-10-19 22:01:47 +0800

    knn.cpp & main.cpp

---
+++ KNearestNeighbors/knn.cpp
@@ -0,0 +1,58 @@
+#include "knn.h"
+#include<map>
+#include<iostream>
+
+
+std::string Knn:getNeighbours(Data d, std::vector<Data> data_list, size_t k){
+    //使用size_t是方便跨平台移植，因为不同平台int的取值范围可能有所不同
+    double *top_k_distances = new double[k];    //分为k类
+    int *top_k_indexes = new int[k];
+    std::fill_n(top_k_indexes, k, -1);    //fill_n的作用是给你一个起始点，然后再给你一个数值count和val，把从起始点开始以此赋予count个元素val的值
+
+    //开始遍历data元素
+    for(size_t index=0;indedx<data_list.size();index++){
+        //计算当前样本d和所有样本之间的距离--l2欧氏距离
+        double distance = d.distance(data_lsit[index]);
+        size_t ind = 0;
+        //寻找当前样本的k个临近的训练集
+        while(ind < k && top_k_indexed[ind]!=-1 && top_k_distances[ind] < distance){
+            //说明此时找到的样本不属于k个距离当前样本d的
+            //且按照升序排列，如1 2 3 4 5
+            ind++;
+        }
+        //如果找到满足k个较近的，进行替换
+        if(ind < k){
+            int shift_index = k-1;
+            while(shift_index > ind){
+                //去掉当前最大的，并把选到的这个样本放进去
+                top_k_indexes[shift_index] = top_k_indexes[shift_index-1];
+                top_k_indexes[shift_index] = top_k_indexes[shift_index-1];
+                shift_index--;
+            }
+            top_k_indexes[ind] = index;
+            top_k_distances[ind] = index;
+        }
+    }
+
+    //开始统计当前样本d近邻的k个元素中最多的类
+    size_t index=0;
+    std::map<std::string, int> top_k_class_map;
+    while(index<k && top_k_indexes[index]!=-1){
+        //遍历近邻的k个元素
+        top_k_class_map[data_list[top_k_indexes[index].cls]]++;
+        index++;
+    }
+    int max = -1;
+    std::string maxClass;
+    for(auto pair:top_k_class_map){
+        if(pair.second > max){
+            max = pair.second;
+            maxClass = pair.first;
+        }
+    }
+    return maxClass;
+}
+
+
+
+
---
+++ KNearestNeighbors/main.cpp
@@ -0,0 +1,135 @@
+#include "knn.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <ctime>
+
+
+
+std::vector<Data> read_iris_data(const std::string &path){
+    std::vector<Data> data;
+    std::ifstream input(path);
+    std::string line;
+    //开始读取数据
+    while(std::getline(input, line)){
+        //逐行读取数据
+        std::stringstream test(line);
+        std::string segment;
+
+        double *fields = new double[10];    //储存特征
+        Data d;
+        d.fields = fields;
+        d.size = 4;
+        int index = 0;
+        while(std::getline(test, segment, ',')){
+            //在一行内以逗号为间隔进行读取
+            if(index == d.size){
+                d.cls = segment;
+                break;
+            }
+            fields[index] = std::stod(segment);
+            index++;
+        }
+        data.push_back(d);
+    }
+    return data;
+}
+
+
+
+
+
+std::pair<std::vector<Data>, std::vector<Data>> split_data(std::vector<Data> data){
+    //划分数据集为训练集、测试集
+    std::vector<Data> training;
+    std::vector<Data> test;
+    size_t percentage70 = data.size()*0.7;
+    size_t index = 0;
+    while(index<percentage70){
+        training.push_back(data[index]);
+        index++;
+    }
+    while(index<data.size()){
+        test.push_back(data[index]);
+        index++;
+    }
+    return std::make_pair(training, test);
+}
+
+
+void normalize(std::vector<Data> data){
+    //正则化特征数据
+    double max = std::numeric_limits<double>::max()    //返回当前系统可取的最大double值
+    double min = std::numeric_limits<double>::min()    //返回当前系统可取的最小double值
+    double *mins = new double[data[0].size()];    //记录所有样本每类特征的最小值
+    double *maxss = new double[data[0].size()];    //记录所有样本每类特征的最大值
+    std::fill_n(mins,data[0].size(),max);
+    std::fill_n(maxes,data[0].size(),min);
+
+    //遍历样本记录mins和maxes
+    for(auto sample:data){
+        for(size_t i=0;i<sample.size();i++){
+            if(sample.fields[i]>maxes[i]){
+                //更新最大值
+                maxes[i] = sample.fields;
+            }
+            if(sample.fields[i]<mins[i]){
+                //更新最小值
+                mins[i] = sample.fields;
+            }
+        }
+    }
+
+    //进行归一化操作
+    for(auto sample:data){
+        for(size_t i=0;i<sample.size();i++){
+            sample.fields[i] = (sample.fields[i]-mins[i])/(maxes[i]-mins[i]);
+        }
+    }
+}
+
+
+
+
+
+void run_knn(std::vector<Data> test, std::vector<Data> training, size_t k){
+    //运行knn分类
+    Knn *knn  = new Knn();
+    //计算测试集的准确率
+    size_t correct = 0;
+    for(auto test_data: test){
+        std::string maxClass = knn->getNeighbours(test_data, training, k);
+        if(maxClass.compare(test_data.cls)==0){
+            //说明此时测试得到的结果与真值相同
+            correct++;
+        }
+    }
+    std::out << correct << " of " << test.size() << "(" << static_cast<double>(100.0) *correct/static_cast<double>(test.size()) << ")" <<std::endl;
+}
+
+
+
+
+//主函数
+int main(int argc, char **argv){
+    std::srand(time(nullptr));
+    auto dataset = read_iris_data("iris.data");
+    normalize(dataset);
+    std::random_shuffle(dataset.begin(),dataset.end());
+    auto splitted_data = split_data(dataset);
+    dataset.clear();
+    run_knn(splitted_data.second, splitted_data.first, 10);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
